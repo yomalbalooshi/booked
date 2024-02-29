8f7229f4c9f5df88642912b3dc9fb1f83d763da3
AbdulAmir1 <abdulamir0@gmail.com>
2024-02-29 12:02:14 +0300

    init commit

---
+++ src/components/About.jsx
@@ -0,0 +1,44 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About Us:</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sint
+        fugit culpa officiis? Quia sapiente saepe corporis deserunt aliquid
+        rerum tenetur ea dignissimos, ipsa debitis, repellendus exercitationem
+        eaque in obcaecati?
+      </p>
+      <h2>Our Mission:</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sint
+        fugit culpa officiis? Quia sapiente saepe corporis deserunt aliquid
+        rerum tenetur ea dignissimos, ipsa debitis, repellendus exercitationem
+        eaque in obcaecati?
+      </p>
+      <h2>Whty Choose Book-A-Stay:</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sint
+        fugit culpa officiis? Quia sapiente saepe corporis deserunt aliquid
+        rerum tenetur ea dignissimos, ipsa debitis, repellendus exercitationem
+        eaque in obcaecati?
+      </p>
+      <h2>Want to Feature your hotels with us?</h2>
+      <section>
+        <div>Contact Us</div>
+        <div>
+          <label htmlFor="name">Company Name</label>
+          <input type="text" id="name" name="name" placeholder="company name" />
+          <label htmlFor="name">Emai</label>
+          <input type="text" id="email" name="email" placeholder="Email" />
+          <label htmlFor="messsage">Message</label>
+          <texarea
+            type="text"
+            id="message"
+            name="message"
+            placeholder="Message"
+          ></texarea>
+        </div>
+      </section>
+    </div>
+  )
+}
