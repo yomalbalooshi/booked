f895e87bb16e80bebf2bd0abcbabbb8ed1f27c81
Mariam Mohammad Ali <maryamhoth@gmail.com>
2024-03-07 06:49:00 +0000

    added popup

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -10,8 +10,34 @@ import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
 import Button from '@mui/material/Button'
 
+import { styled } from '@mui/material/styles'
+import Dialog from '@mui/material/Dialog'
+import DialogTitle from '@mui/material/DialogTitle'
+import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions'
+
+import CloseIcon from '@mui/icons-material/Close'
+import Typography from '@mui/material/Typography'
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2)
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1)
+  }
+}))
+
 const SignIn = ({ setUser, signin }) => {
   let navigate = useNavigate()
+  const [open, setOpen] = useState(false)
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+
   const [formValues, setFormValues] = useState({ email: '', password: '' })
 
   const handleChange = (e) => {
@@ -20,10 +46,14 @@ const SignIn = ({ setUser, signin }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const payload = await signin(formValues)
-    setFormValues({ email: '', password: '' })
-    setUser(payload)
-    navigate('/')
+    try {
+      const payload = await signin(formValues)
+      setFormValues({ email: '', password: '' })
+      setUser(payload)
+      navigate('/')
+    } catch (error) {
+      handleClickOpen()
+    }
   }
   const [showPassword, setShowPassword] = useState(false)
 
@@ -90,6 +120,37 @@ const SignIn = ({ setUser, signin }) => {
             >
               Log In
             </Button>
+            <BootstrapDialog
+              onClose={handleClose}
+              aria-labelledby="customized-dialog-title"
+              open={open}
+            >
+              <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+                Log In Error
+              </DialogTitle>
+              <IconButton
+                aria-label="close"
+                onClick={handleClose}
+                sx={{
+                  position: 'absolute',
+                  right: 8,
+                  top: 8,
+                  color: (theme) => theme.palette.grey[500]
+                }}
+              >
+                <CloseIcon />
+              </IconButton>
+              <DialogContent dividers>
+                <Typography gutterBottom>
+                  The provided credentials don't match an account in our system.
+                </Typography>
+              </DialogContent>
+              <DialogActions>
+                <Button autoFocus onClick={handleClose}>
+                  ok
+                </Button>
+              </DialogActions>
+            </BootstrapDialog>
           </div>
         </form>
       </div>
